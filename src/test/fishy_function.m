17b370bc672b09320044b923585e7b8698464143
rohailamalik <rohailahmadmalik@hotmail.com>
2025-07-31 02:47:23 +0300

    incorporated conversions between MATLAB and Python

--- src/test/fishy_function.m
+++ src/test/fishy_function.m
@@ -1,4 +1,4 @@
 function sum_result = fishy_function(varargin)
     % fishy_function accepts multiple numbers as input and returns their sum
-    sum_result = sum([varargin{:}]);
+    sum_result =? sum([varargin{:}])
 end
