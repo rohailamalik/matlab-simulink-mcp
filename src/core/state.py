06bb706372951fed4b52f1287f1b5e7d6ff87748
rohailamalik <rohailahmadmalik@hotmail.com>
2025-08-04 02:26:04 +0300

    Temp canvas incorporation. State update

--- src/core/state.py
+++ src/core/state.py
@@ -4,12 +4,27 @@ import matlab.engine
 
 @dataclass
 class ServerState:
-    session_name: Optional[str] = None
+    session: Optional[str] = None
     eng: Optional[matlab.engine.MatlabEngine] = None
     sl_lib_data: Optional[dict] = None
+    canvas_path: Optional[str] = None
+    helpers_path: Optional[str] = None
     
 
 server_state = ServerState()
+
+def get_state(key: str):
+     
+     if not hasattr(server_state, key):
+        raise AttributeError(f"'{key}' is not a valid key.")
+
+     value = getattr(server_state, key)
+     
+     if value is None:
+        raise ValueError(f"'{key}' has not been set yet.")
+     
+     return value
+
 
 def get_engine():
     if server_state.eng is None:
@@ -21,3 +36,5 @@ def get_sl_lib_data():
     if server_state.sl_lib_data is None:
         raise RuntimeError("Server data is not loaded. Please start the server first.")
     return server_state.sl_lib_data
+
+
--- src/core/tools.py
+++ src/core/tools.py
@@ -3,13 +3,14 @@ import inspect
 import sys
 import tempfile
 from difflib import get_close_matches
-from core.state import get_engine, get_sl_lib_data 
+from core.state import get_state 
 from utils.converters import fetch, batch_fetch
 from langchain_core.tools import tool, BaseTool
+from pathlib import Path
 
 
-
-def open_simulink_file(file_name: str, get_content: bool = True, open_in_desktop: bool = False) -> dict | Exception:
+@tool
+def open_simulink_file(file_name: str, get_content: bool = True, open_in_desktop: bool = False) -> dict | str:
     """
     Opens a Simulink file and/or returns its content.
 
@@ -22,16 +23,13 @@ def open_simulink_file(file_name: str, get_content: bool = True, open_in_desktop
         If asked, description of the system inside the Simulink file as a dictionary.
     """
 
-    eng = get_engine()
-
-    if eng is None:
-        return RuntimeError("MATLAB engine is not available.")
+    eng = get_state("eng")
 
     if not file_name.endswith(".slx"):
-        return ValueError("Only .slx files are supported.")
+        raise ValueError("Only .slx files are supported.")
 
     if eng.exist(file_name, "file") == 0:
-        return FileNotFoundError(f"'{file_name}' not found in MATLAB's current working directory.")
+        raise FileNotFoundError(f"'{file_name}' not found in MATLAB's current working directory.")
 
     eng.load_system(file_name)
 
@@ -40,25 +38,25 @@ def open_simulink_file(file_name: str, get_content: bool = True, open_in_desktop
             if eng.usejava("desktop"):
                 eng.open_system(file_name, nargout=0)
             else:
-                return RuntimeError("Failed to open in desktop. MATLAB desktop is not running")
+                raise RuntimeError("Failed to open in desktop. MATLAB desktop is not running")
         except Exception as e:
-            return RuntimeError(f"Unexpected error while opening Simulink file in desktop: {e}")
+            raise RuntimeError(f"Unexpected error while opening Simulink file in desktop: {e}")
 
     if get_content:
         try:
             content = eng.describe_system(file_name) 
             return content
         except Exception as e:
-            return RuntimeError(f"Unexpected error while reading Simulink file: {e}")
+            raise RuntimeError(f"Unexpected error while reading Simulink file: {e}")
 
     else:
         return f"{file_name} opened successfully in MATLAB desktop."
 
 
-
-def open_matlab_file(file_name: str, get_content: bool = True, open_in_desktop: bool = False) -> str | Exception:
+@tool
+def open_matlab_file(file_name: str, get_content: bool = True, open_in_desktop: bool = False) -> str:
     """
-    Opens a MATLAB script and/or returns its content.
+    Returns the content of a matlab script file and/or opens it in MATLAB desktop.
 
     Parameters:
         file_name: script file name with .m extension
@@ -69,36 +67,34 @@ def open_matlab_file(file_name: str, get_content: bool = True, open_in_desktop: 
         If asked, code inside the script as a string.
     """
 
-    eng = get_engine()
-
-    if eng is None:
-        return RuntimeError("MATLAB engine is not available.")
+    eng = get_state("eng")
 
     if not file_name.endswith(".m"):
-        return ValueError("Only .m files can be opened.")
+        raise ValueError("Only .m files can be opened.")
     
     if eng.exist(file_name, "file") == 0:
-        return FileNotFoundError(f"'{file_name}' not found in MATLAB's current working directory.")
+        raise FileNotFoundError(f"'{file_name}' not found in MATLAB's current working directory.")
     
     if open_in_desktop:
         try:
             if eng.usejava("desktop"):
                 eng.edit(file_name, nargout=0)
             else:
-                return RuntimeError("Failed to open in desktop.")
+                raise RuntimeError("Failed to open. MATLAB desktop is not running.")
         except Exception as e:
-            return RuntimeError(f"Unexpected error while opening MATLAB file in desktop: {e}")
+            raise RuntimeError(f"Unexpected error while opening MATLAB file in desktop: {e}")
 
     if get_content: 
         try:
-            content = eng.fileread(file_name, nargout=1)
+            return eng.fileread(file_name, nargout=1) 
         except Exception as e:
-            return RuntimeError(f"Unexpected error while reading MATLAB file: {e}") 
+            raise RuntimeError(f"Unexpected error while reading MATLAB file: {e}") 
     else:
         return f"{file_name} opened successfully in MATLAB desktop."
 
 
-def save_matlab_code(code: str, file_name: str, overwrite: bool = False) -> str | Exception:
+@tool
+def save_matlab_code(code: str, file_name: str, overwrite: bool = False) -> str:
     """
     Validates and saves MATLAB code to a .m file in the current MATLAB working directory.
 
@@ -108,92 +104,70 @@ def save_matlab_code(code: str, file_name: str, overwrite: bool = False) -> str 
         overwrite: whether to overwrite if the file already exists. False by default
 
     Returns:
-        Confirmation message if saved. Errors if validation fails.
+        Confirmation message with validation errors if any.
     """
 
-    eng = get_engine()
-
-    if eng is None:
-        return RuntimeError("MATLAB engine is not available.")
+    eng = get_state("eng")
 
     if not file_name.endswith(".m"):
-        return ValueError("Please save as a .m file.")
+        raise ValueError("Please save as a .m file.")
+    
+    if not file_name.isidentifier():
+        raise ValueError("File name must be a valid MATLAB identifier.")
     
     if not overwrite and eng.exist(file_name, "file") == 2: 
-        return FileNotFoundError(f"'{file_name}' already exists in MATLAB's current working directory.")
+        raise FileNotFoundError(f"'{file_name}' already exists in MATLAB's current working directory.")
 
-    cwd = eng.pwd() 
-    file_path = os.path.join(cwd, file_name)
+    file_path = Path(eng.pwd()) / file_name
 
     try:
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(code)
     except Exception as e:
-        return RuntimeError(f"Error saving the code file {file_name} in directory {cwd}: {e}")
+        raise RuntimeError(f"Error saving the code file {file_name} in current working directory: {e}")
     
     try: 
         issues = eng.validate_code(file_path)
     except Exception as e:
-            return RuntimeError(f"Error validating the code file {file_name}: {e}") 
-    finally:
-        if issues:
-            os.remove(file_path)
-            return ValueError(f"MATLAB syntax issues found in '{file_name}':\n" + "\n".join(issues)
-        )
+        raise RuntimeError(f"Error validating the code file {file_name}: {e}") 
+    
+    if issues:
+        return f"{file_name} saved but failed validation with following errors:\n" + "\n".join(issues)
 
     return f"Code validated and saved successfully as {file_name}."
 
 
-def run_matlab_code(code: str, variables: list[str] = None, convert= False) -> dict | str | Exception:
+@tool
+def run_matlab_code(code: str, variables: list[str] = None, convert= False) -> str:
     """
-    Executes MATLAB code, and retrieves values of specified variables from workspace.
+    Executes MATLAB code, and returns whatever is displayed in the MATLAB command window, wrapped in a string.
 
     Parameters:
         code: MATLAB code to run.
-        variables: list of variable names to retrieve from current MATLAB workspace after the code is ran. None by default. 
-        convert: whether to convert the variables to Python types. When False, the values will be returned as string-wrapped MATLAB types. False by default.
 
     Returns:
-        If asked, a dictionary of asked variables and their values.
+        The results printed to MATLAB command window, wrapped in a string.
     """
 
-    eng = get_engine()
-
-    if eng is None:
-        return RuntimeError("MATLAB engine is not available.")
-
-    temp = False
+    eng = get_state("eng")
+    canvas_path = get_state("canvas_path")
 
     try:
-        # eval does not support multi-line code, so we write it to a temporary file, run it and then delete it.
+        # eval does not support multi-line code, so we write it to the canvas (i.e. a temporary file) and run it through evalc.
+        # evalc also returns whatever is printed to command window as a string, so it helps with disp functions etc.
         # TODO: Later it will be replaced by a canvas-based editor.
-        with tempfile.NamedTemporaryFile(mode="w", suffix=".m", delete=False) as f:
+        with open(canvas_path, "w", encoding="utf-8") as f:
             f.write(code)
-            temp_path = f.name
-            temp = True
 
-        eng.run(temp_path, nargout=0)
+        output = eng.evalc(f"run('{canvas_path}')", nargout = 1)
 
     except Exception as e:
-        return RuntimeError(f"MATLAB execution failed: {e}")
+        raise RuntimeError(f"MATLAB execution failed: {e}")
     
-    finally:
-        # Whatever happens, delete the temporary file post execution.
-        # temp ensures that the file is deleted only if it was created.
-        # This prevents deletion of a file with same/ similar name in the current directory.
-        if temp and os.path.exists(temp_path):
-            try:
-                os.remove(temp_path)
-            except Exception as e: # TODO replace this with debugging logger
-                raise RuntimeError(f"Warning: Failed to delete temporary file {temp_path}: {e}")
+    return output
 
-    if variables:
-        return batch_fetch(eng, variables, convert=convert)
-    else:
-        return "Code executed successfully."
-
-
-def get_variables(variables: list[str], convert = False) -> dict | Exception:
+@tool
+def get_variables(variables: list[str], convert = False) -> dict:
     """
     Fetches specified variables from MATLAB workspace.
 
@@ -205,32 +179,43 @@ def get_variables(variables: list[str], convert = False) -> dict | Exception:
         A dictionary of asked variables and their values.
     """
 
-    eng = get_engine()
+    eng = get_state("eng")
 
-    if eng is None:
-        return RuntimeError("MATLAB engine is not available.")
+    result = {}
 
-    return batch_fetch(eng, variables, convert=convert)
+    for var in variables:
+        try:
+            # Cannot do "if in eng.workspace" since it is not a native Python dict, rather a MATLAB object with some dict-like properties.
+            if eng.exist(var, "var") == 1:
+                if convert:
+                    result[var] = fetch(eng, var)
+                else:
+                    result[var] = eng.evalc(f"{var}", nargout=1)
+            else:
+                result[var] = "Variable not found in MATLAB workspace."
+        
+        except Exception as e:
+            raise RuntimeError(f"Error fetching variable '{var}': {e}")
+        
+    return result
+    
 
-
-def get_workspace_summary() -> str | Exception:
+@tool
+def get_workspace_summary() -> str:
     """
     Returns a summary of current MATLAB workspace: variable names, sizes, and types.
     """
 
-    eng = get_engine()
-
-    if eng is None:
-        return RuntimeError("MATLAB engine is not available.")
+    eng = get_state("eng")
 
     try:
         info = eng.workspace_summary(nargout=1)
     except Exception as e:
-        return RuntimeError(f"Failed to get workspace summary: {e}")
+        raise RuntimeError(f"Failed to get workspace summary: {e}")
 
     return info
 
-
+@tool
 def search_library(query) -> dict:
     """
     Searches for a block name in the Simulink block library and returns all matching source paths.
@@ -242,9 +227,7 @@ def search_library(query) -> dict:
         A list of paths of blocks with similar names.
     """
 
-    sl_lib_data = get_sl_lib_data()
-    if sl_lib_data is None:
-        return RuntimeError("Simulink library data not found.")
+    sl_lib_data = get_state("sl_lib_data")
 
     n = 2
     cutoff = 0.5
@@ -253,8 +236,9 @@ def search_library(query) -> dict:
         block_names = list(sl_lib_data.keys())
         matches = get_close_matches(query, block_names, n=n, cutoff=cutoff)
         return {name: sl_lib_data[name]['paths'] for name in matches}
+    
     except Exception as e:
-        return RuntimeError(f"Error searching Simulink library: {e}")
+       raise RuntimeError(f"Error searching Simulink library: {e}")
 
 
 # Collect all the tools 
